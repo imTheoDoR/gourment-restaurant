6139c2eb4a740f86cf1bdf909bf5204c6edb3340
TheoDoR <teodorzabulica@gmail.com>
2024-03-19 16:25:00 +0200

    responsive meet element

--- app/(homepage)/page.tsx
+++ app/(homepage)/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <BestDeal />
       </section>
       <ExploreMenu />
-      {/* <MeetWithChef /> */}
+      <MeetWithChef />
       {/* <WhyUs /> */}
       {/* <UnforgettableWrapp /> */}
       {/* <TestimonialWrapper /> */}
--- components/meet/meet-container.tsx
+++ components/meet/meet-container.tsx
@@ -8,21 +8,21 @@ import Delimiter from "@/components/delimiter";
 
 const MeetWithChef = () => {
   return (
-    <section className="bg-meet bg-cover w-full h-auto overflow-y-hidden">
+    <section className="bg-meet bg-cover w-full h-auto overflow-y-hidden pb-20 lg:pb-0">
       <div className="container flex justify-center">
-        <div className="flex items-center justify-stretch pt-20 gap-0 lg:gap-20">
+        <div className="flex flex-col lg:flex-row items-center justify-stretch pt-20 gap-0 lg:gap-20">
           <Image
             src="/images/chef.png"
             alt="gourment restaurant main chef"
             width={550}
             height={694}
-            className="-mb-16"
+            className="-mb-16 w-56 lg:w-96"
           />
 
           <div className="relative">
             <div className="w-12 h-12 bg-orange3 rounded-full border-[6px] border-white absolute -bottom-5 -left-5 z-10" />
 
-            <div className="bg-white2 min-w-[500px] max-w-xl py-10 px-20 rounded-t-[18px] rounded-br-[18px] border-b-4 border-orange2/40 z-50 relative">
+            <div className="bg-white2 max-w-xl py-10 px-5 lg:px-10 rounded-t-[18px] rounded-br-[18px] border-b-4 border-orange2/40 z-50 relative">
               <h4 className="font-colus text-3xl text-center">
                 Meet with Chef&apos;s
               </h4>
@@ -32,7 +32,7 @@ const MeetWithChef = () => {
                 team of chefs at Gourmet Restaurant.
               </p>
 
-              <div className="mt-10 flex justify-between items-center">
+              <div className="mt-10 flex flex-col lg:flex-row gap-5 lg:gap-0 justify-between items-center">
                 <div className="flex flex-row-reverse group">
                   <Link href="#meet-chefs">
                     <div className="w-16 h-16 bg-orange2 hover:bg-red duration-300 rounded-full flex items-center justify-center shadow-md shadow-black/30 -ml-7">
@@ -60,7 +60,7 @@ const MeetWithChef = () => {
                     />
                   </div>
 
-                  <div className="rounded-full flex items-center justify-center shadow-md shadow-black/30 -ml-7 overflow-hidden">
+                  <div className="rounded-full flex items-center justify-center shadow-md shadow-black/30 overflow-hidden">
                     <Image
                       src="/images/chef1.png"
                       alt="gourment restaurant chef one"
@@ -70,6 +70,7 @@ const MeetWithChef = () => {
                     />
                   </div>
                 </div>
+
                 <div>
                   <Button className="bg-red text-white h-16 px-16 hover:bg-green shadow-md">
                     Meet With Chef&apos;s
