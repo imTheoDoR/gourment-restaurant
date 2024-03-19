744b4f536de8a6d7cd7935fdb174a817de254018
TheoDoR <teodorzabulica@gmail.com>
2024-03-19 14:41:38 +0200

    head more responsive

--- components/header.tsx
+++ components/header.tsx
@@ -7,14 +7,14 @@ import { LuCalendarClock } from "react-icons/lu";
 const Header = () => {
   return (
     <div className="h-screen w-full bg-hero bg-cover bg-center bg-no-repeat relative overflow-hidden">
-      <div className="relative lg:absolute left-0 right-0">
+      <div className="absolute left-0 right-0">
         <Navbar />
       </div>
-      <div className="w-full h-full text-center flex flex-col justify-stretch lg:last:justify-center items-center pt-20">
+      <div className="w-full h-full text-center flex flex-col justify-center items-center">
         <h1 className="font-colus text-white2 tracking-wide uppercase text-5xl lg:text-7xl drop-shadow-lg">
           Welcome to gourment,
         </h1>
-        <p className="max-w-max lg:max-w-2xl text-2xl lg:text-4xl mx-auto pt-16 lg:pt-24 font-bold drop-shadow-lg text-white2">
+        <p className="max-w-max lg:max-w-2xl text-2xl lg:text-4xl mx-auto px-5 lg:px-0 pt-16 lg:pt-24 font-bold drop-shadow-lg text-white2">
           Book your table now and let us take you on a culinary adventure like
           no other!
         </p>
